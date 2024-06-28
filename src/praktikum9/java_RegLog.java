611024a0d5659ffbf29068917f77406c8949f161
Rafi Imanullah <rifkypermana964@gmail.com>
2024-06-28 18:47:32 +0700

    Initial commit

---
+++ src/praktikum9/java_RegLog.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package praktikum9;
+
+/**
+ *
+ * @author User
+ */
+public class java_RegLog {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativetTo(null);
+    }
+    
+}
